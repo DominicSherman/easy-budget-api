ee5d37b347920f02ff735e1bd5a883da13d96ab9
Dominic Sherman <dominic.sherman98@gmail.com>
2019-11-24 06:12:07 -0800

    add jest config file

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'collectCoverageFrom': [
+        '<rootDir>/src/*/**/*.ts',
+        '<rootDir>/src/*.ts'
+    ],
+    'coverageDirectory': '.coverage',
+    'coveragePathIgnorePatterns': [
+        '<rootDir>/src/generated/graphql.ts'
+    ],
+    'coverageThreshold': {
+        'global': {
+            'branches': 100,
+            'functions': 100,
+            'lines': 100,
+            'statements': 100
+        }
+    },
+    'moduleFileExtensions': [
+        'ts',
+        'tsx',
+        'js',
+        'json'
+    ],
+    'testMatch': [
+        '<rootDir>/__tests__/**/*.test.ts'
+    ],
+    'transform': {
+        '\\.(ts|tsx)$': 'ts-jest'
+    }
+};
